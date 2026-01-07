1108f039db17612ea9d5e2abbe3e54af191ee6e1
frantic03117 <shahrukh@franticpro.com>
2026-01-07 12:17:27 +0530

    remove duplicated

--- app/Http/Controllers/API/Customer/ProductsApiController.php
+++ app/Http/Controllers/API/Customer/ProductsApiController.php
@@ -66,7 +66,7 @@ class ProductsApiController extends Controller
         }
         $seller_ids = CommonHelper::getSellerIds($request->latitude, $request->longitude);
         //seller_ids is working tested
-        $variants = SellerProduct::whereIn('seller_id', $seller_ids)->pluck('product_id')->toArray();
+        $variants = SellerProduct::whereIn('seller_id', $seller_ids)->distinct('product_id')->pluck('product_id')->toArray();
         return response()->json(['data' => $variants]);
     }
     public function getProducts(Request $request)
